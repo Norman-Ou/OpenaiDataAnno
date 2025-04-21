b0a3195f76bc5021ef5438e8d1e34a6a431dc151
Norman-Ou <ouruizhe@bupt.edu.cn>
2025-04-21 07:23:49 +0000

    test success

---
+++ test.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "developer", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+print(completion.choices[0].message)
